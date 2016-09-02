bcd92b9a697dd39483778e22bccc191197e15332
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-09-02 16:04:23 +1000

    Added method to get all user folders

--- RevitVersionLogger/Program.cs
+++ RevitVersionLogger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace RevitVersionLogger
     {
         static void Main(string[] args)
         {
+            var userFolders = GetAllUserFolders();
+        }
+
+        private static List<string> GetAllUserFolders()
+        {
+            var usersToIgnore = new string[] { "administrator", "ajc", "ajcadmin", "caduser", "all users", "default", "default user", "public" };
+            var folders = Directory.EnumerateDirectories(@"C:\Users\").ToList();
+
+            var userFolders = new List<string>();
+
+            foreach (var dir in folders)
+            {
+                var directoryUser = dir.Substring(9).Trim().ToLower();
+
+                for (int i = 0; i < usersToIgnore.Length; i++)
+                {
+                    if (usersToIgnore[i].Equals(directoryUser))
+                    {
+                        break;
+                    }
+                    else if (i == usersToIgnore.Length - 1)
+                    {
+                        userFolders.Add(dir);
+                    }
+                }
+            }
+
+            return userFolders;
         }
     }
 }
