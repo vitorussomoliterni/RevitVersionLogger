2b91c188cce2799c0a0149ee803f1dc7d95319aa
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-09-02 16:41:43 +1000

    Added method to get the latest journal file

--- RevitVersionLogger/Program.cs
+++ RevitVersionLogger/Program.cs
@@ -12,33 +12,51 @@ namespace RevitVersionLogger
         static void Main(string[] args)
         {
             var userFolders = GetAllUserFolders();
+            GetRevitVersion(userFolders);
+        }
+
+        private static void GetRevitVersion(List<string> userFolders)
+        {
+            foreach (var user in userFolders)
+            {
+                var userName = user.Substring(9);
+                var journalsPath = user + @"\AppData\Local\Autodesk\Revit\Autodesk Revit 2016\Journals";
+                try
+                {
+                    if (Directory.Exists(journalsPath))
+                    {
+                        var journalFile = GetLatestJournal(journalsPath);
+
+                        if (journalFile == null)
+                        {
+                            throw new FileNotFoundException("No journals found in {0} folder", userName);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+        }
+
+        private static string GetLatestJournal(string path)
+        {
+            var journals = Directory.EnumerateFiles(path, "journal.*.txt", SearchOption.TopDirectoryOnly);
+
+            var latestJournal = journals.LastOrDefault();
+            return latestJournal;
         }
 
         private static List<string> GetAllUserFolders()
         {
-            var usersToIgnore = new string[] { "administrator", "ajc", "ajcadmin", "caduser", "all users", "default", "default user", "public" };
+            var usersToIgnore = new HashSet<string> { "administrator", "ajc", "ajcadmin", "caduser", "all users", "default", "default user", "public" };
             var folders = Directory.EnumerateDirectories(@"C:\Users\").ToList();
 
-            var userFolders = new List<string>();
+            folders.RemoveAll(f => usersToIgnore.Contains(f.Substring(9).Trim().ToLower()));
 
-            foreach (var dir in folders)
-            {
-                var directoryUser = dir.Substring(9).Trim().ToLower();
-
-                for (int i = 0; i < usersToIgnore.Length; i++)
-                {
-                    if (usersToIgnore[i].Equals(directoryUser))
-                    {
-                        break;
-                    }
-                    else if (i == usersToIgnore.Length - 1)
-                    {
-                        userFolders.Add(dir);
-                    }
-                }
-            }
-
-            return userFolders;
+            return folders;
         }
     }
 }
