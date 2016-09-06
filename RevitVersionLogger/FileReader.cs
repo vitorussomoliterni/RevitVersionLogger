ce99439508042bf87e089f51c90c37c325b1654d
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-09-06 18:02:16 +1000

    Implemented method to extract revit version from the journal txt file

--- RevitVersionLogger/FileReader.cs
+++ RevitVersionLogger/FileReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,35 @@ namespace RevitVersionLogger
 {
     public static class FileReader
     {
+        public static string GetRevitVersion(string path)
+        {
+            StreamReader file = new StreamReader(path);
+            string line;
 
+            try
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains("Build"))
+                    {
+                        break;
+                    }
+                }
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    return null;
+                }
+
+                var version = line.Substring(9, 13);
+
+                return version;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+        }
     }
 }
