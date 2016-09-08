a6307c5a66b46d83c2709bd0f63b2bf46ff2967f
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-09-08 15:14:33 +1000

    Added switch

--- RevitVersionLogger/RevitInfo.cs
+++ RevitVersionLogger/RevitInfo.cs
@@ -24,7 +24,46 @@ namespace RevitVersionLogger
 
         private string ConvertBuildIntoRevitVersion(string buildNumber)
         {
-            throw new NotImplementedException();
+            string version;
+
+            switch (buildNumber)
+            {
+                case "20150220_1215":
+                    version = "First Customer Ship,16.0.428.0";
+                    break;
+                case "20150506_1715":
+                    version = "Service Pack 1,16.0.462.0";
+                    break;
+                case "20150714_1515":
+                    version = "Service Pack 2,16.0.490.0";
+                    break;
+                case "20151007_0715":
+                    version = "Release 2 (R2),16.0.1063";
+                    break;
+                case "20151209_0715":
+                    version = "Update 1 for R2,16.0.1092.0";
+                    break;
+                case "20160126_1600":
+                    version = "Update 2 for R2,16.0.1108.0";
+                    break;
+                case "20160217_1800":
+                    version = "Update 3 for R2,16.0.1118.0";
+                    break;
+                case "20160314_0715":
+                    version = "Update 4 for R2,16.0.1124.0";
+                    break;
+                case "20160525_1230":
+                    version = "Update 5 for R2,16.0.1144.0";
+                    break;
+                case "20160720_0715":
+                    version = "Update 6 for R2,16.0.1161.0";
+                    break;
+                default:
+                    version = "No version found for given build number.";
+                    break;
+            }
+
+            return version;
         }
 
         private string GetMostUpdateJournal(List<string> journalsList)
